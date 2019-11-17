8e9db4736b31ea0594c78cf99f5f1610b7dcb1a4
Jérémie Foucault <j.foucault@foogere.fr>
2019-11-17 05:35:58 +0200

    fix: Dynamic file

--- api/app.py
+++ api/app.py
@@ -32,9 +32,23 @@ def create_app():
   @app.route('/getFashion', methods=['POST'])
   def get_fashion_suggest():
     data = request.json
-    os.system('cd classification/data_dict/shape_and_feature/ && ./scripts/edit_and_visualize_demo.sh 3.jpg shape_and_texture False 0 13 0.25')
-    # subprocess.call(['./scripts/edit_and_visualize_demo.sh 2.jpg shape_and_texture True 0 10 0.25'], '../classification/data_dict/shape_and_feature/')
-    return send_from_directory('/home/jeremie/FashionPlus/classification/data_dict/shape_and_feature/results/demo/images/', 'reconstructed_3.jpg')
+    file = data.get('filename')
+    step_sz = data.get('step_sz')
+    max_iter = data.get('max_iter')
+    auto_swap = data.get('auto_swap')
+    swapped_partid = data.get('swapped_partid')
+    if file not in ['3.jpg', '4.jpg', '18.jpg']:
+      return custom_response({'error': 'File not found'}, 404)
+    if step_sz < 0 or step_sz > 1 :
+        return custom_response({'error': 'step_sz should be between 0 and 1'}, 400)
+    if auto_swap not in ['True', 'False'] :
+      return custom_response({'error': 'auto_swap must be True or False'}, 400)
+    # if swapped_partid not in []:
+    #       pass    
+    print(type(swapped_partid))
+    print(type(step_sz))
+    os.system('cd classification/data_dict/shape_and_feature/ && ./scripts/edit_and_visualize_demo.sh ' + file +' shape_and_texture False 0 13 0.25')
+    return send_from_directory('/home/jeremie/FashionPlus/classification/data_dict/shape_and_feature/results/demo/images/', 'final_' + file)
 
   
 
