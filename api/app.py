8d0c8a970f20dd254e1f0197a08a054d14e4c605
Jérémie Foucault <j.foucault@foogere.fr>
2019-11-17 05:52:00 +0200

    feat: Add parameters in payload

--- api/app.py
+++ api/app.py
@@ -33,21 +33,23 @@ def create_app():
   def get_fashion_suggest():
     data = request.json
     file = data.get('filename')
-    step_sz = data.get('step_sz')
-    max_iter = data.get('max_iter')
-    auto_swap = data.get('auto_swap')
-    swapped_partid = data.get('swapped_partid')
+    step_sz = data.get('step_sz', 0.25)
+    max_iter = data.get('max_iter', 10)
+    auto_swap = data.get('auto_swap', 'True')
+    update_type = data.get('update_type', 'shape_and_texture')
+    swapped_partid = data.get('swapped_partid', 0)
     if file not in ['3.jpg', '4.jpg', '18.jpg']:
       return custom_response({'error': 'File not found'}, 404)
     if step_sz < 0 or step_sz > 1 :
         return custom_response({'error': 'step_sz should be between 0 and 1'}, 400)
     if auto_swap not in ['True', 'False'] :
       return custom_response({'error': 'auto_swap must be True or False'}, 400)
-    # if swapped_partid not in []:
-    #       pass    
-    print(type(swapped_partid))
-    print(type(step_sz))
-    os.system('cd classification/data_dict/shape_and_feature/ && ./scripts/edit_and_visualize_demo.sh ' + file +' shape_and_texture False 0 13 0.25')
+    if swapped_partid not in [4, 5, 6, 7, 0]:
+          return custom_response({'error': 'swapped_partid must be in 4, 5, 6, 7, 0'}, 400)
+    if update_type not in ['shape_and_texture', 'shape_only', 'texture_only']:
+      return custom_response({'error': 'update_type must be in shape_and_texture, shape_only, texture_only'}, 400)
+
+    os.system('cd classification/data_dict/shape_and_feature/ && ./scripts/edit_and_visualize_demo.sh ' + file + ' ' + update_type + ' ' + auto_swap + ' ' + swapped_partid + ' ' + max_iter + ' ' + step_sz)
     return send_from_directory('/home/jeremie/FashionPlus/classification/data_dict/shape_and_feature/results/demo/images/', 'final_' + file)
 
   
